ddd2ea1db79540e7ab6f4446db5cc7eeeec74cd5
lucaslpfa <85973415+lucaslpfa@users.noreply.github.com>
2021-06-24 19:14:01 -0300

    Add files via upload

---
+++ months.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() 
+{
+    int A = 0;
+    string mes;
+    
+    cin>>A;
+    
+    if(A==1)
+    {
+        mes = "January";
+    }
+    else if(A==2)
+    {
+        mes = "February";
+    }
+     else if(A==3)
+    {
+        mes = "March";
+    }
+     else if(A==4)
+    {
+        mes = "April";
+    }
+     else if(A==5)
+    {
+        mes = "May";
+    }
+     else if(A==6)
+    {
+        mes = "June";
+    }
+     else if(A==7)
+    {
+        mes = "July";
+    }
+     else if(A==8)
+    {
+        mes = "August";
+    }
+     else if(A==9)
+    {
+        mes = "September";
+    }
+     else if(A==10)
+    {
+        mes = "October";
+    }
+     else if(A==11)
+    {
+        mes = "November";
+    }
+     else if(A==12)
+    {
+        mes = "December";
+    }
+
+cout << mes << endl;
+
+}    
+    
+ 
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
